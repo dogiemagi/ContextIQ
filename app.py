dda1ec888fb77fb39bba38b07f5487be7bb95746
Mageshwari_M <151842994+dogiemagi@users.noreply.github.com>
2026-08-17 12:21:09 +0530

    Refactor imports and update text splitter import

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask_cors import CORS
 from groq import Groq
 
 from langchain_community.document_loaders import CSVLoader, PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
 
